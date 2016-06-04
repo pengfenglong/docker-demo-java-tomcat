1630886c314af48fac53759fcd1019833ce24ab0
pp88sky <pp88sky@163.com>
2016-06-04 15:54:01 +0800

    add

--- src/main/java/com/smartdp/rest/controller/MovieController.java
+++ src/main/java/com/smartdp/rest/controller/MovieController.java
@@ -28,36 +28,36 @@ public class MovieController {
 		
 		List<Map<String, String>> list = new ArrayList<Map<String,String>>();
 		// 载入驱动  
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			// 建立连接  
-			Connection con = DriverManager.getConnection(  
-			  "jdbc:mysql://10.10.26.58:3306/rWGc8qiFbSaRouO1", "uDMLaE7XRohHspWG", "pvcipx3Ty7RfqYrg2");  
-			// 创建状态  
-			Statement stmt = con.createStatement();  
-			// 执行SQL语句，返回结果集  
-			ResultSet rs = stmt.executeQuery("SELECT * FROM test");  
-			// 对结果集进行处理  
-			while (rs.next()) {  
-			 int _id = rs.getInt("id");  
-			 String name = rs.getString("name");  
-			 Map<String, String> m = new HashMap<String, String>();
-			 
-			 m.put("id", _id + "");
-			 m.put("name", name + "");
-			 
-			 list.add(m);
-			}  
-			// 释放资源  
-			stmt.close();  
-			con.close();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}  
+//		try {
+//			Class.forName("com.mysql.jdbc.Driver");
+//			// 建立连接  
+//			Connection con = DriverManager.getConnection(  
+//			  "jdbc:mysql://10.10.26.58:3306/rWGc8qiFbSaRouO1", "uDMLaE7XRohHspWG", "pvcipx3Ty7RfqYrg2");  
+//			// 创建状态  
+//			Statement stmt = con.createStatement();  
+//			// 执行SQL语句，返回结果集  
+//			ResultSet rs = stmt.executeQuery("SELECT * FROM test");  
+//			// 对结果集进行处理  
+//			while (rs.next()) {  
+//			 int _id = rs.getInt("id");  
+//			 String name = rs.getString("name");  
+//			 Map<String, String> m = new HashMap<String, String>();
+//			 
+//			 m.put("id", _id + "");
+//			 m.put("name", name + "");
+//			 
+//			 list.add(m);
+//			}  
+//			// 释放资源  
+//			stmt.close();  
+//			con.close();
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}  
 		return list;
 		
 	}
