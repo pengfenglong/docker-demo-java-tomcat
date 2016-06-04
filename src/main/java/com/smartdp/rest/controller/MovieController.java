0a9e8d48ee1bf674bd9c49aaedc3785466101086
pp88sky <pp88sky@163.com>
2016-06-04 12:51:57 +0800

    add

--- src/main/java/com/smartdp/rest/controller/MovieController.java
+++ src/main/java/com/smartdp/rest/controller/MovieController.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,32 +20,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/rest/movie")
 public class MovieController {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	@RequestMapping(value = "view/{id}", method = RequestMethod.GET)
 	@ResponseBody
 	public Object getMovie(@PathVariable("id") Long id, Model model) {
-		String catalog = "";
-		// 载入驱动
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection(
-					"jdbc:mysql://10.10.26.58:3306/rWGc8qiFbSaRouO1", "uDMLaE7XRohHspWG", "pvcipx3Ty7RfqYrg2");
-			catalog = con.getCatalog();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// 建立连接
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		model.addAttribute("movie", id);
-		Map<String, String> m = new HashMap<String, String>();
-		m.put("a", "a");
-		m.put("b", "b");
-		m.put("catalog", catalog);
-		return m;
+		
+		List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from test");
+		
+		return list;
 
 	}
 
