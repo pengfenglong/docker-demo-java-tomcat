9b89b20da789f8a8a5610647f27fcc87a0cab79a
pp88sky <pp88sky@163.com>
2016-06-04 16:51:11 +0800

    add

--- src/main/java/com/smartdp/rest/controller/MovieController.java
+++ src/main/java/com/smartdp/rest/controller/MovieController.java
@@ -1,12 +1,6 @@
 package com.smartdp.rest.controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
