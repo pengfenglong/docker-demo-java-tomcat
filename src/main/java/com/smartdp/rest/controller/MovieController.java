fc141fceda897f78dc07d9a1e961f968ab7237b5
pp88sky <pp88sky@163.com>
2016-06-04 11:18:24 +0800

    ADD

--- src/main/java/com/smartdp/rest/controller/MovieController.java
+++ src/main/java/com/smartdp/rest/controller/MovieController.java
@@ -1,5 +1,8 @@
 package com.smartdp.rest.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,13 +20,30 @@ public class MovieController {
 	@RequestMapping(value = "view/{id}", method = RequestMethod.GET)
 	@ResponseBody
 	public Object getMovie(@PathVariable("id") Long id, Model model) {
+		String catalog = "";
+		// 载入驱动
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(
+					"jdbc:mysql://10.10.26.58:3306/rWGc8qiFbSaRouO1", "uDMLaE7XRohHspWG", "pvcipx3Ty7RfqYrg2");
+			catalog = con.getCatalog();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// 建立连接
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		model.addAttribute("movie", id);
 		Map<String, String> m = new HashMap<String, String>();
 		m.put("a", "a");
 		m.put("b", "b");
+		m.put("catalog", catalog);
 		return m;
 
 	}
-	
+
 }
