ab3522148005fdebef84a183ab6ac2b65d5c4c4e
pp88sky <pp88sky@163.com>
2016-06-04 16:06:02 +0800

    add

--- src/main/java/com/smartdp/rest/controller/MovieController.java
+++ src/main/java/com/smartdp/rest/controller/MovieController.java
@@ -2,14 +2,13 @@ package com.smartdp.rest.controller;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,45 +20,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/rest/movie")
 public class MovieController {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 	
 	@RequestMapping(value = "view/{id}", method = RequestMethod.GET)
 	@ResponseBody
 	public Object getMovie(@PathVariable("id") Long id, Model model) {
 		
-		List<Map<String, String>> list = new ArrayList<Map<String,String>>();
-		// 载入驱动  
-//		try {
-//			Class.forName("com.mysql.jdbc.Driver");
-//			// 建立连接  
-//			Connection con = DriverManager.getConnection(  
-//			  "jdbc:mysql://10.10.26.58:3306/rWGc8qiFbSaRouO1", "uDMLaE7XRohHspWG", "pvcipx3Ty7RfqYrg2");  
-//			// 创建状态  
-//			Statement stmt = con.createStatement();  
-//			// 执行SQL语句，返回结果集  
-//			ResultSet rs = stmt.executeQuery("SELECT * FROM test");  
-//			// 对结果集进行处理  
-//			while (rs.next()) {  
-//			 int _id = rs.getInt("id");  
-//			 String name = rs.getString("name");  
-//			 Map<String, String> m = new HashMap<String, String>();
-//			 
-//			 m.put("id", _id + "");
-//			 m.put("name", name + "");
-//			 
-//			 list.add(m);
-//			}  
-//			// 释放资源  
-//			stmt.close();  
-//			con.close();
-//		} catch (ClassNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}  
-		return list;
 		
+		
+		
+		List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from test");
+		
+		return list;
+
 	}
 
 }
